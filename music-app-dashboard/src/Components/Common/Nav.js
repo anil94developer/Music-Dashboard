a7ef4d8f7f6f7faea5b19d1abc520a7b4cc52039
anil kumar <143709450+Ab2software@users.noreply.github.com>
2024-10-23 21:21:46 +0530

    nav add

--- music-app-dashboard/src/Components/Common/Nav.js
+++ music-app-dashboard/src/Components/Common/Nav.js
@@ -49,16 +49,16 @@ export const Nav = () => {
 
                 <section class="sidebar">
 
-                    {/* <div class="user-panel">
+                    <div class="user-panel">
                         <div class="pull-left image">
-                            <img src="./dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
+                            <img src="../../../public/css/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
                         </div>
                         <div class="pull-left info">
                             <p>rahul</p>
 
                             <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                         </div>
-                    </div> */}
+                    </div>
 
                     <ul class="sidebar-menu">
 
