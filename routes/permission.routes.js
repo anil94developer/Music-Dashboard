06b99456ef47d90b2cfb89ec450d9df2ab1fabb5
anil kumar <143709450+Ab2software@users.noreply.github.com>
2024-11-25 22:07:47 +0530

    test

--- routes/permission.routes.js
+++ routes/permission.routes.js
@@ -4,7 +4,7 @@ const verifyToken = require("../utils/verifyToken");
 const permission = require("../services/permissionService")
 
 router.post('/add-permission',verifyToken,permission.add);
-router.get("/list-permission",verifyToken,permission.listWithUserDetails)
-router.get("/list",verifyToken,permission.list)
+router.get("/my-permission",verifyToken,permission.listWithUserDetails)
+router.get("/my-user-list",verifyToken,permission.list)
 
 module.exports = router 
